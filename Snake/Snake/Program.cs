4917e57d70b33a7cb4bb04c082d1464f49d2749f
darcia89 <shabanov89@gmail.com>
2017-02-21 21:54:20 +0300

    Box 20x20 constructed

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -15,7 +15,19 @@ namespace Snake
 
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
-                                  
+
+            HorizontalLine H1line = new HorizontalLine(20, 40, 20, '+');
+            H1line.Draw();
+
+            HorizontalLine H2line = new HorizontalLine(20, 40, 40, '+');
+            H2line.Draw();
+
+            VerticalLine V1line = new VerticalLine(20, 40, 20, '+');
+            V1line.Draw();
+
+            VerticalLine V2line = new VerticalLine(20, 40, 40, '+');
+            V2line.Draw();
+
             Console.ReadLine();
         }
 
